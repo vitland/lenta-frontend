9000034c046c21e8529480c0aab0bdba32ba9882
Vitkor <vitland@gmail.com>
2023-10-03 14:35:15 +0300

    feat: add filter slice

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+type ForecastItem = {
+  [date: string]: number
+}
+
+export type Forecast = {
+  store: string
+  sku: string
+  group: string
+  cat: string
+  subcat: string
+  forecast_date: string
+  forecast: ForecastItem
+}
+
+export type ForecastState = {
+  forecastList: Forecast[]
+  status: string
+  error: string | undefined
+}
+
+export type FiltersState = {
+  shopsSelected: string[]
+  groupsSelected: string[]
+  categoriesSelected: string[]
+  subcategoriesSelected: string[]
+  skuSelected: string[]
+  status: string
+  error: string | undefined
+}
+
+export type caterogiesResponse = {
+  category: string
+  group: string
+  subcategory: string
+  sku: string
+  uom: number
+}
+
+export type selectedValues = {
+  value: string[]
+  label: string[]
+}
