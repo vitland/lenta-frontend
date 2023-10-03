ce3b236989f4d9e0123854e5e91f9a5500a3a1d8
Vitkor <vitland@gmail.com>
2023-10-03 14:36:29 +0300

    feat: add forecast slice

---
+++ src/features/forecast/forecastSlice.ts
@@ -0,0 +1,58 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { Forecast, ForecastState } from "../../types"
+import { RootState } from "../../app/store"
+import axios from "axios"
+
+const initialState: ForecastState = {
+  forecastList: [],
+  status: "",
+  error: "",
+}
+
+export const fetchForecast = createAsyncThunk(
+  "forecats/fetchForecast",
+  async (query) => {
+    const response = await axios.get("URL DOBAVIT/forecats")
+    return [...response.data]
+  },
+)
+// export const exportForecast = createAsyncThunk(
+//   "forecats/fetchForecast",
+//   async (query) => {
+//     const response = await axios.get("URL DOBAVIT/exportforecats")
+//     return [...response.data]
+//   },
+// )
+
+const forecastSlice = createSlice({
+  name: "forecast",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchForecast.pending, (state, action) => {
+        state.status = "loading"
+      })
+      .addCase(fetchForecast.fulfilled, (state, action) => {
+        state.status = "succeeded"
+      })
+      .addCase(fetchForecast.rejected, (state, action) => {
+        state.status = "failed"
+        state.error = action.error.message
+      })
+
+      // .addCase(exportForecast.pending, (state, action) => {
+      //   state.status = "loading"
+      // })
+      // .addCase(exportForecast.fulfilled, (state, action) => {
+      //   state.status = "succeeded"
+      // })
+      // .addCase(exportForecast.rejected, (state, action) => {
+      //   state.status = "failed"
+      //   state.error = action.error.message
+      // })
+  },
+})
+
+export const selectAllFilters = (state: RootState) => state.filters
+export default forecastSlice.reducer
