c9d9d381c7643fdfb77c96703af0a08e41d2342f
Viktor Kikodze <vitland@gmail.com>
2023-10-08 11:11:18 +0300

    feat: add Statistic page

--- src/pages/Stratistics.tsx
+++ src/pages/Stratistics.tsx
@@ -1,7 +1,35 @@
+import Form from "../components/form/Form"
 import Header from "../components/header/Header"
+import styles from "./Statistics.module.css"
+import { data } from "../utils/data"
+import Table from "../Table"
 
 function Statistics() {
-  return <Header title="Статистика" />
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    // dispatch(setFakeData(data))
+    // dispatch(fetchForecast({ skus, shops, date }))
+  }
+
+  return (
+    <>
+      <Header title="Статистика" />
+      <section className={styles.container}>
+        <Form handleSubmit={handleSubmit} />
+
+        {data && (
+          <p>{`Прогноз спроса на ${
+            Object.keys(data[0].forecast.sales_units)[0]
+          } - ${
+            Object.keys(data[0].forecast.sales_units)[
+              Object.keys(data[0].forecast.sales_units).length - 1
+            ]
+          }`}</p>
+        )}
+        {data && <Table data={data} />}
+      </section>
+    </>
+  )
 }
 
 export default Statistics
